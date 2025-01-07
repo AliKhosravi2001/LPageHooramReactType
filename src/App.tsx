5bcd477823d0cac177363d869edf0e1e7e009937
Ali Khosravi <ali.kh137924@gmail.com>
2025-01-08 00:46:10 +0330

    login

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import {
 } from "@/components/ui/carousel";
 
 import { Button } from "@/components/ui/button";
-import logohooram from "@/assets/horam.jpg";
+import logohooram from "@/assets/horam.png";
 import texthooram from "@/assets/Hooram.png";
 import "@/assets/fontawesome-free-6.5.2-web/css/all.css";
 import {
@@ -30,10 +30,13 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 function App() {
   return (
@@ -50,20 +53,31 @@ function App() {
                   <SheetTitle>شرکت بازرگانی تجارت ماندگار هورام</SheetTitle>
                   <SheetDescription>
                     <Dialog>
-                      <DialogTrigger>
+                      <DialogTrigger asChild>
                         <Button>ورود / ثبت نام</Button>
                       </DialogTrigger>
-                      <DialogContent>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <div className="flex justify-center items-center w-[100%]">
+                          <img
+                            src={logohooram}
+                            alt="لوگو تجارت ماندگار هورام"
+                            className="h-20 pointer-events-none"
+                          />
+                        </div>
                         <DialogHeader>
-                          <DialogTitle>ورود</DialogTitle>
+                          <DialogTitle> ورود | ثبت نام</DialogTitle>
                           <DialogDescription>
-                            فعلا در دسترس نیست بعدا تلاش کنید
-                            <br />
-                            eror 404
-                            <br />
-                            Not found
+                            لطفا شماره موبایل یا ایمیل خود را وارد کنید
                           </DialogDescription>
                         </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                          <div className="grid grid-cols-4 items-center gap-4 ">
+                            <Input id="user" className="col-span-full" />
+                          </div>
+                        </div>
+                        <DialogFooter>
+                          <Button type="submit">ورود</Button>
+                        </DialogFooter>
                       </DialogContent>
                     </Dialog>
                     <Button>
@@ -117,20 +131,31 @@ function App() {
         <div className="grow mx-2  my-2 w-96 flex justify-end items-center">
           <div className=" min-h-12 min-w-32 hidden xl:block">
             <Dialog>
-              <DialogTrigger>
+              <DialogTrigger asChild>
                 <Button>ورود / ثبت نام</Button>
               </DialogTrigger>
-              <DialogContent>
+              <DialogContent className="sm:max-w-[425px]">
+                <div className="flex justify-center items-center w-[100%]">
+                  <img
+                    src={logohooram}
+                    alt="لوگو تجارت ماندگار هورام"
+                    className="h-20 pointer-events-none"
+                  />
+                </div>
                 <DialogHeader>
-                  <DialogTitle>ورود</DialogTitle>
+                  <DialogTitle> ورود | ثبت نام</DialogTitle>
                   <DialogDescription>
-                    فعلا در دسترس نیست بعدا تلاش کنید
-                    <br />
-                    eror 404
-                    <br />
-                    Not found
+                    لطفا شماره موبایل یا ایمیل خود را وارد کنید
                   </DialogDescription>
                 </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4 ">
+                    <Input id="user" className="col-span-full" />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <Button type="submit">ورود</Button>
+                </DialogFooter>
               </DialogContent>
             </Dialog>
           </div>
