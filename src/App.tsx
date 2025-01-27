c0ece5ea5635f74979924a95361981b6ed4374ca
Ali Khosravi <ali.kh137924@gmail.com>
2025-01-27 11:39:44 +0330

    startRegex

--- src/App.tsx
+++ src/App.tsx
@@ -21,120 +21,122 @@ import HeadLand from "./components/HeadLand/HeadLand";
 function App() {
   return (
     <>
-      <HeadLand />
-      <div className="flex justify-center absolute top-20">
-        <Carousel
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-          plugins={[
-            Autoplay({
-              delay: 3000,
-            }),
-          ]}
-        >
-          <CarouselContent>
-            <CarouselItem>
-              <div className="relative">
-                <img
-                  src={alibaba}
-                  alt="خرید از علی بابا"
-                  className="pointer-events-none brightness-50"
-                />
-                <div className="absolute top-0 flex justify-center items-center w-full h-full">
-                  <a href="">
-                    <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
-                      خرید از چین
-                    </h1>
-                  </a>
+      <div className="font-estedad">
+        <HeadLand />
+        <div className="flex justify-center absolute top-20">
+          <Carousel
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+            plugins={[
+              Autoplay({
+                delay: 3000,
+              }),
+            ]}
+          >
+            <CarouselContent>
+              <CarouselItem>
+                <div className="relative">
+                  <img
+                    src={alibaba}
+                    alt="خرید از علی بابا"
+                    className="pointer-events-none brightness-50"
+                  />
+                  <div className="absolute top-0 flex justify-center items-center w-full h-full">
+                    <a href="">
+                      <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
+                        خرید از چین
+                      </h1>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </CarouselItem>
-            <CarouselItem>
-              <div className="relative">
-                <img
-                  src={Foreign_currency_payment}
-                  alt="پرداخت ارزی"
-                  className="pointer-events-none brightness-50"
-                />
-                <div className="absolute top-0 flex justify-center items-center w-full h-full">
-                  <a href="">
-                    <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
-                      پرداخت ارزی
-                    </h1>
-                  </a>
+              </CarouselItem>
+              <CarouselItem>
+                <div className="relative">
+                  <img
+                    src={Foreign_currency_payment}
+                    alt="پرداخت ارزی"
+                    className="pointer-events-none brightness-50"
+                  />
+                  <div className="absolute top-0 flex justify-center items-center w-full h-full">
+                    <a href="">
+                      <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
+                        پرداخت ارزی
+                      </h1>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </CarouselItem>
-            <CarouselItem>
-              <div className="relative">
-                <img
-                  src={International_shipping}
-                  alt="حمل و نقل بین المللی"
-                  className="pointer-events-none brightness-50"
-                />
-                <div className="absolute top-0 flex justify-center items-center w-full h-full">
-                  <a href="">
-                    <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
-                      حمل و نقل بین المللی
-                    </h1>
-                  </a>
+              </CarouselItem>
+              <CarouselItem>
+                <div className="relative">
+                  <img
+                    src={International_shipping}
+                    alt="حمل و نقل بین المللی"
+                    className="pointer-events-none brightness-50"
+                  />
+                  <div className="absolute top-0 flex justify-center items-center w-full h-full">
+                    <a href="">
+                      <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
+                        حمل و نقل بین المللی
+                      </h1>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </CarouselItem>
-            <CarouselItem>
-              <div className="relative">
-                <img
-                  src={Türkiye}
-                  alt="خرید از ترکیه"
-                  className="pointer-events-none brightness-50"
-                />
-                <div className="absolute top-0 flex justify-center items-center w-full h-full">
-                  <a href="">
-                    <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
-                      خرید از ترکیه
-                    </h1>
-                  </a>
+              </CarouselItem>
+              <CarouselItem>
+                <div className="relative">
+                  <img
+                    src={Türkiye}
+                    alt="خرید از ترکیه"
+                    className="pointer-events-none brightness-50"
+                  />
+                  <div className="absolute top-0 flex justify-center items-center w-full h-full">
+                    <a href="">
+                      <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
+                        خرید از ترکیه
+                      </h1>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </CarouselItem>
-            <CarouselItem>
-              <div className="relative">
-                <img
-                  src={UAE}
-                  alt="خرید از امارات"
-                  className="pointer-events-none brightness-50"
-                />
-                <div className="absolute top-0 flex justify-center items-center w-full h-full">
-                  <a href="">
-                    <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
-                      خرید از امارات
-                    </h1>
-                  </a>
+              </CarouselItem>
+              <CarouselItem>
+                <div className="relative">
+                  <img
+                    src={UAE}
+                    alt="خرید از امارات"
+                    className="pointer-events-none brightness-50"
+                  />
+                  <div className="absolute top-0 flex justify-center items-center w-full h-full">
+                    <a href="">
+                      <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
+                        خرید از امارات
+                      </h1>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </CarouselItem>
-            <CarouselItem>
-              <div className="relative">
-                <img
-                  src={Wholesale_and_single_purchase}
-                  alt="خرید عمده و خرید تک"
-                  className="pointer-events-none brightness-50"
-                />
-                <div className="absolute top-0 flex justify-center items-center w-full h-full">
-                  <a href="">
-                    <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
-                      خرید عمده و خرید تک
-                    </h1>
-                  </a>
+              </CarouselItem>
+              <CarouselItem>
+                <div className="relative">
+                  <img
+                    src={Wholesale_and_single_purchase}
+                    alt="خرید عمده و خرید تک"
+                    className="pointer-events-none brightness-50"
+                  />
+                  <div className="absolute top-0 flex justify-center items-center w-full h-full">
+                    <a href="">
+                      <h1 className=" text-center text-white md:text-6xl sm:text-5xl text-2xl">
+                        خرید عمده و خرید تک
+                      </h1>
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </CarouselItem>
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
       </div>
     </>
   );
